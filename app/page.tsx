f2679e856419a181a24956a68d1d1dbcaf291499
izzybiz-s <iadmin@eesybiz.systems>
2025-09-01 21:01:40 +0300

    dadad

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,46 @@
-import { Button } from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/Cta";
 import React from "react";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">welcome to my sass app</h1>
-      <Button>Lets start</Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the explorer"
+          topic="Neural network of the brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+          bookmarked={false}
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number wizard"
+          topic="Derivatives & integrals"
+          subject="science"
+          duration={30}
+          color="#e5d0ff"
+          bookmarked={false}
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="language"
+          subject="English Litrature"
+          duration={30}
+          color="#bde7ff"
+          bookmarked={false}
+        />
+      </section>
+      <section className="home-section">
+        <CompanionsList />
+        <CTA />
+      </section>
+    </main>
   );
 };
 
