ab40994fdffa1fccc43bcd2e3b5aa6af8594f734
izzybiz-s <iadmin@eesybiz.systems>
2025-09-10 15:34:56 +0300

    implement recent sessions

--- app/page.tsx
+++ app/page.tsx
@@ -2,45 +2,34 @@ import CompanionCard from "@/components/CompanionCard";
 import CompanionsList from "@/components/CompanionsList";
 import CTA from "@/components/Cta";
 import { recentSessions } from "@/constants";
-import React from "react";
+import {
+  getAllCompanions,
+  getRecentSessions,
+} from "@/lib/actions/companion.actions";
+import { getSubjectColor } from "@/lib/utils";
 
-const Page = () => {
+const Page = async () => {
+  const companions = await getAllCompanions({ limit: 3 });
+  const recentSessionsCompanions = await getRecentSessions(10);
+
   return (
     <main>
       <h1>Popular Companions</h1>
+
       <section className="home-section">
-        <CompanionCard
-          id="123"
-          name="Neura the explorer"
-          topic="Neural network of the brain"
-          subject="science"
-          duration={45}
-          color="#ffda6e"
-          bookmarked={false}
-        />
-        <CompanionCard
-          id="456"
-          name="Countsy the Number wizard"
-          topic="Derivatives & integrals"
-          subject="science"
-          duration={30}
-          color="#e5d0ff"
-          bookmarked={false}
-        />
-        <CompanionCard
-          id="789"
-          name="Verba the Vocabulary Builder"
-          topic="language"
-          subject="English Litrature"
-          duration={30}
-          color="#bde7ff"
-          bookmarked={false}
-        />
+        {companions.map((companion) => (
+          <CompanionCard
+            key={companion.id}
+            {...companion}
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
       </section>
+
       <section className="home-section">
         <CompanionsList
-          title="Completed Companions"
-          companions={recentSessions}
+          title="Recently completed sessions"
+          companions={recentSessionsCompanions}
           classNames="w-2/3 max-lg:w-full"
         />
         <CTA />
