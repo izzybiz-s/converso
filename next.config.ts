5f87e60d685d6a043f727816490d21b3c9133664
izzybiz-s <iadmin@eesybiz.systems>
2025-09-13 11:00:34 +0300

    add ignore build errors to next conf

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
-import {withSentryConfig} from "@sentry/nextjs";
+import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images:{
-    remotePatterns:[
-      {hostname:'img.clerk.com'}
+  ignoreBuildErrors: true,
+  images: {
+    remotePatterns: [
+      { hostname: 'img.clerk.com' }
     ]
   }
   /* config options here */
